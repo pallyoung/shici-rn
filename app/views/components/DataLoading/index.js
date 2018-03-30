a1fa0d4d62ab8b365e6933c9d55004cbabdd0791
Spencer <pallyoung@163.com>
2018-03-30 17:28:32 +0800

    图片

---
+++ app/views/components/DataLoading/index.js
@@ -0,0 +1,19 @@
+import React,{Component} from 'react';
+
+import {
+    View,
+    Text
+    Image
+} from 'react-native';
+
+
+class DataLoading extends Component{
+    constructor(...props) {
+        super(...props)
+    }
+    render(){
+
+    }
+}
+
+export default DataLoading;
